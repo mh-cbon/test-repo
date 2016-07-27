95a74092733e006dc888a23818aee9e05bedc986
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-27 19:30:44 +0200

    test

--- createrepo.sh
+++ createrepo.sh
@@ -1,6 +1,6 @@
 BASEURL=$1
 yum install createrepo -y
-cd /docker/yum/i386
+cd /docker/rpm/i386
 createrepo . --no-database -u ${BASEURL}i386/ -i ../../i386.list
-cd /docker/yum/x86_64
+cd /docker/rpm/x86_64
 createrepo . --no-database -u ${BASEURL}x86_64/ -i ../../x86_64.list
